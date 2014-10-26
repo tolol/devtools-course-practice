90fb6562f98368dfb5bdb1cf12040a806e069f90
shochin-alexandr <alexandr.shochin@gmail.com>
2014-10-26 17:18:16 +0300

    My 1 Backup

---
+++ code/shochin-alexandr/include/tconv.h
@@ -0,0 +1,20 @@
+// Copyright 2014 Shochin Alexandr
+
+#ifndef CODE_SHOCHIN_ALEXANDR_LAB2_INCLUDE_TCONV_H_
+#define CODE_SHOCHIN_ALEXANDR_LAB2_INCLUDE_TCONV_H_
+
+class TemperatureConverter {
+ private:
+	 double Celsius_;
+ public:
+	 TemperatureConverter(double Celsius);
+	 ~TemperatureConverter();
+	 double Convert(int unit);
+ protected:
+	 int Check();
+	 double GetKelvin();
+	 double GetFahrenheit();
+	 double GetNewton();
+};
+
+#endif  // CODE_SHOCHIN_ALEXANDR_LAB2_INCLUDE_TCONV_H_
---
+++ code/shochin-alexandr/test/test_tconv.cpp
@@ -0,0 +1,55 @@
+// Copyright 2014 Shochin Alexandr
+
+#include <gtest/gtest.h>
+
+#include <limits.h>
+#include <string>
+
+#include "include/tcalc.h"
+
+class TemperatureConverterTest : public ::testing::Test {
+ protected:
+    TemperatureConverter tc(-300);
+    TemperatureConverter tc2(100);
+    TemperatureConverter tc3(200);
+};
+
+TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong) {
+    EXPECT_THROW(tc.Convert(1) , std::string);
+}
+
+TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong) {
+    EXPECT_THROW(tc.Convert(2) , std::string);
+}
+
+TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong) {
+    EXPECT_THROW(tc.Convert(3) , std::string);
+}
+
+TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong) {
+    EXPECT_THROW(tc2.Convert(0) , std::string);
+}
+
+TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong) {
+    EXPECT_THROW(tc2.Convert(0) , std::string);
+}
+
+TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong) {
+    EXPECT_THROW(tc2.Convert(0) , std::string);
+}
+
+TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_unit_Is_Wrong) {
+    EXPECT_THROW(tc.Convert(4) , std::string);
+}
+
+TEST_F(TConvTest, Check_the_value_of_Kelvin) {
+    EXPECT_THROW(tc3.Convert(1) , 473.14999999999998);
+}
+
+TEST_F(TConvTest, Check_the_value_of_Fahrenheit) {
+    EXPECT_THROW(tc3.Convert(2) , 392.00000000000000);
+}
+
+TEST_F(TConvTest, Check_the_value_of_Newton) {
+    EXPECT_THROW(tc3.Convert(3) , 66.000000000000000);
+}
