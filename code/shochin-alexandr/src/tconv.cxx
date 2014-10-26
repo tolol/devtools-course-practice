f390db01be099ec8f40f09fe6b44bdde2f692038
shochin-alexandr <alexandr.shochin@gmail.com>
2014-10-26 18:53:14 +0300

    Fixed syntax

--- code/shochin-alexandr/src/tconv.cxx
+++ code/shochin-alexandr/src/tconv.cxx
@@ -43,7 +43,11 @@ double TemperatureConverter::Convert(int unit) {
             case 2: return GetFahrenheit();
             case 3: return GetNewton();
             }
-        } else throw std::string("take on integer values 1, 2 or 3");
-    } else throw std::string(" can not be below -273.15");
+        } else {
+            throw std::string("take on integer values 1, 2 or 3");
+        }
+    } else {
+        throw std::string(" can not be below -273.15");
+    }
     return NULL;
 }
