1e12892f48b5fe52a7ece9c01b629f4ad498ea07
shochin-alexandr <alexandr.shochin@gmail.com>
2014-10-26 20:16:33 +0300

    Fixed

--- code/shochin-alexandr/test/test_tconv.cpp
+++ code/shochin-alexandr/test/test_tconv.cpp
@@ -9,51 +9,44 @@
 
 class TConvTest : public ::testing::Test {
  protected:
-    double a = -300.0;
-    double b = 100.0;
-    double c = 200.0;
-
-    TemperatureConverter tc(a);
-    TemperatureConverter tc2(b);
-    TemperatureConverter tc3(c);
 };
 
 TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong_1) {
+    TemperatureConverter tc(-300.0);
     EXPECT_THROW(tc.Convert(1) , std::string);
 }
 
 TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong_2) {
+    TemperatureConverter tc(-300.0);
     EXPECT_THROW(tc.Convert(2) , std::string);
 }
 
 TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong_3) {
+    TemperatureConverter tc(-300.0);
     EXPECT_THROW(tc.Convert(3) , std::string);
 }
 
-TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_unit_Is_Wrong_1) {
-    EXPECT_THROW(tc2.Convert(0) , std::string);
-}
-
-TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_unit_Is_Wrong_2) {
-    EXPECT_THROW(tc2.Convert(0) , std::string);
-}
-
 TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_unit_Is_Wrong_3) {
+    TemperatureConverter tc2(100.0);
     EXPECT_THROW(tc2.Convert(0) , std::string);
 }
 
 TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_unit_Is_Wrong_4) {
+    TemperatureConverter tc(-300.0);
     EXPECT_THROW(tc.Convert(4) , std::string);
 }
 
 TEST_F(TConvTest, Check_the_value_of_Kelvin) {
-    EXPECT_THROW(tc3.Convert(1) , 473.14999999999998);
+    TemperatureConverter tc3(200.0);
+    EXPECT_EQ(tc3.Convert(1) , 473.14999999999998);
 }
 
 TEST_F(TConvTest, Check_the_value_of_Fahrenheit) {
-    EXPECT_THROW(tc3.Convert(2) , 392.00000000000000);
+    TemperatureConverter tc3(200.0);
+    EXPECT_EQ(tc3.Convert(2) , 392.00000000000000);
 }
 
 TEST_F(TConvTest, Check_the_value_of_Newton) {
-    EXPECT_THROW(tc3.Convert(3) , 66.000000000000000);
+    TemperatureConverter tc3(200.0);
+    EXPECT_EQ(tc3.Convert(3) , 66.000000000000000);
 }
