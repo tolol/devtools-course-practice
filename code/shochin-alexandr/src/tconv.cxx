8516897729f23519cf0505b13517bc7fa3d3c93c
shochin-alexandr <alexandr.shochin@gmail.com>
2014-10-26 19:21:47 +0300

    Fixed syntax

--- code/shochin-alexandr/src/tconv.cxx
+++ code/shochin-alexandr/src/tconv.cxx
@@ -5,8 +5,7 @@
 #include <limits>
 #include <string>
 
-TemperatureConverter::TemperatureConverter(double Celsius) {
-     this->Celsius_ = Celsius;
+TemperatureConverter::TemperatureConverter(double Celsius) : this->Celsius_(Celsius) {
 }
 
 TemperatureConverter::~TemperatureConverter() {
