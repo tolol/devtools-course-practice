e5e53e5b7af2e4ddb666b45ca485e9b81f705add
Tolol <Tolol1594@gmail.com>
2014-10-27 18:39:14 +0400

    Fixed whitespaces

--- code/Tolstolutskaya-Olga/test/test_3DVector.cpp
+++ code/Tolstolutskaya-Olga/test/test_3DVector.cpp
@@ -5,26 +5,26 @@
 #include "include/3DVektor.h"
 
 
-TEST_F(3DVektorTest, Can_Create) {
+TEST(3DVektorTest, Can_Create) {
     3DVektor a(3, 4, 5);
     EXPECT_EQ(3, a.GetA());
     EXPECT_EQ(4, a.GetB());
     EXPECT_EQ(5, a.GetC());
 }
 
-TEST_F(3DVektorTest, Can_Create_Default) {
+TEST(3DVektorTest, Can_Create_Default) {
     3DVektor a();
     EXPECT_EQ(0, a.GetA());
     EXPECT_EQ(0, a.GetB());
     EXPECT_EQ(0, a.GetC());
 }
 
-TEST_F(3DVektorTest, Correct_Computation_Norm) {
+TEST(3DVektorTest, Correct_Computation_Norm) {
     3DVektor a(1, 2, 3);
     EXPECT_EQ(3.741657, a.Norma());
 }
 
-TEST_F(3DVektorTest, Correct_Computation_Normalize) {
+TEST(3DVektorTest, Correct_Computation_Normalize) {
     3DVektor a(1, 2, 3);
     a.Normalize();
     EXPECT_EQ(0.267261, a.GetA());
@@ -32,13 +32,13 @@ TEST_F(3DVektorTest, Correct_Computation_Normalize) {
     EXPECT_EQ(0.801784, a.GetC());
 }
 
-TEST_F(3DVektorTest, Correct_Computation_ScalarProduct) {
+TEST(3DVektorTest, Correct_Computation_ScalarProduct) {
     3DVektor a(1, 2, 3);
     3DVektor b(2, 1, -2);
     EXPECT_EQ(-2, a->ScalarProduct(b));
 }
 
-TEST_F(3DVektorTest, Correct_Computation_VectorProduct) {
+TEST(3DVektorTest, Correct_Computation_VectorProduct) {
     3DVektor a(1, 2, 3);
     3DVektor b(2, 1, -2);
     3DVektor c(2, 1, -2);
