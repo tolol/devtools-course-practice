c909fbce190d0b4a31390e0835ca7ac3a28671aa
Tolol <Tolol1594@gmail.com>
2014-10-27 17:17:06 +0400

    Lab 3D Vector

---
+++ code/Tolstolutskaya-Olga/include/3DVektor.h
@@ -0,0 +1,36 @@
+// Copyright 2014 Tolstolutskaya Olga
+#ifndef CODE_TOLSTOLUTSKAYA_OLGA_INCLUDE_3DVECTOR
+#define CODE_TOLSTOLUTSKAYA_OLGA_INCLUDE_3DVECTOR
+
+#include <stdio.h>
+#include <math.h>
+
+class Vector3d {
+ private:
+    double a;
+    double b;
+    double c;  
+
+ public:
+
+    Vector3d();
+
+    Vector3d(double _a, double _b, double _c);
+
+    double GetC();
+
+    double GetB();
+
+    double GetA();
+
+    double Norma();
+
+    void Normalize();
+
+    double ScalarProduct(Vector3d *Vector);
+
+    Vector3d* VectorProduct(Vector3d *Vector);
+
+    void Print();
+};
+#endif // CODE_TOLSTOLUTSKAYA_OLGA_INCLUDE_3DVECTOR
---
+++ code/Tolstolutskaya-Olga/src/3DVektors.cxx
@@ -0,0 +1,60 @@
+// Copyright 2014 Tolstolutskaya Olga
+//Операции над 3D векторами: норма, нормализация, скалярное и векторное произведение
+#include <stdio.h>
+#include <math.h>
+#include "include/3DVektor.h"
+
+Vector3d::Vector3d() {
+    a=0;
+    b=0;
+    c=0;
+}
+
+Vector3d::Vector3d(double _a, double _b, double _c) {
+    a=_a;
+    b=_b;
+    c=_c;
+}
+
+double Vector3d::GetA() {
+    return(a);
+}
+
+double Vector3d::GetB() {
+    return(b);
+}
+
+double Vector3d::GetC() {
+    return(c);
+}
+
+double Vector3d::Norma() { 
+    double LocalNorm;
+    double ForSqrt = a*a+b*b+c*c;
+    LocalNorm = sqrt(ForSqrt);
+    return(LocalNorm);
+}
+
+void Vector3d::Normalize() {
+    if((a!=0)||(b!=0)||(c!=0)){
+        double LocalNorm = Norma();
+        double inversion=1/LocalNorm;
+        a = a*inversion;
+        b = b*inversion;
+        c = c*inversion;
+    }
+    else printf("error Vector 0");
+}
+
+double Vector3d::ScalarProduct(Vector3d *Vector) {
+    double SProduct = Vector->a*a+Vector->b*b+Vector->c*c;
+    return (SProduct);
+}
+
+Vector3d* Vector3d::VectorProduct(Vector3d *Vector) {
+    Vector3d *VProduct = new Vector3d();
+    VProduct->a = b*Vector->c-c*Vector->b;
+    VProduct->b = -(a*Vector->c-c*Vector->a);
+    VProduct->c = a*Vector->b-b*Vector->a;
+    return(VProduct);
+}
