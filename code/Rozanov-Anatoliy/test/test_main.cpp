ea0e7236a8dabe068cafa397ca8a911d35002535
RozanovAnatoliy <rozantolya@mail.ru>
2014-10-28 21:46:57 +0400

    Added test_main.cpp

---
+++ code/Rozanov-Anatoliy/test/test_main.cpp
@@ -0,0 +1,11 @@
+// Copyright 2014 Anatoliy Rozanov
+
+#include <gtest/gtest.h>
+#include <stdio.h>
+
+#include "include/interpolationsearch.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
