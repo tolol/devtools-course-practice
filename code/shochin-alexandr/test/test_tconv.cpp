6f884a4a14d5ef9198694ce620af032f41991ada
shochin-alexandr <alexandr.shochin@gmail.com>
2014-10-31 18:21:09 +0300

    Fixed test

--- code/shochin-alexandr/test/test_tconv.cpp
+++ code/shochin-alexandr/test/test_tconv.cpp
@@ -23,20 +23,20 @@ TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_Celsius_Is_Wrong_1) {
 
 TEST_F(TConvTest, Do_Throw_Exception_When_Value_For_unit_Is_Wrong_1) {
     TemperatureConverter tc2(100.0);
-    EXPECT_NEAR(tc2.Convert(0) , 0.0, , 1e-5);
+    EXPECT_NEAR(tc2.Convert(0) , 0.0 , 1e-5);
 }
 
 TEST_F(TConvTest, Check_the_value_of_Kelvin) {
     TemperatureConverter tc3(200.0);
-    EXPECT_NEAR(tc3.Convert(1) , 473.14999999999998, 1e-5);
+    EXPECT_NEAR(tc3.Convert(1) , 473.14999999999998 , 1e-5);
 }
 
 TEST_F(TConvTest, Check_the_value_of_Fahrenheit) {
     TemperatureConverter tc3(200.0);
-    EXPECT_NEAR(tc3.Convert(2) , 392.00000000000000, 1e-5);
+    EXPECT_NEAR(tc3.Convert(2) , 392.00000000000000 , 1e-5);
 }
 
 TEST_F(TConvTest, Check_the_value_of_Newton) {
     TemperatureConverter tc3(200.0);
-    EXPECT_NEAR(tc3.Convert(3) , 66.000000000000000, 1e-5);
+    EXPECT_NEAR(tc3.Convert(3) , 66.000000000000000 , 1e-5);
 }
