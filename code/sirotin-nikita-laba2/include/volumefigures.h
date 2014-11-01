e28e0eb2072591468f12c27907718210a4701ff1
Nikita94 <niksir20@mail.ru>
2014-11-01 13:24:23 +0400

    Fixed volumefigures.h file

--- code/sirotin-nikita-laba2/include/volumefigures.h
+++ code/sirotin-nikita-laba2/include/volumefigures.h
@@ -8,10 +8,10 @@ class Volume {
   Volume();
   ~Volume();
 
-  double Cube(double side);
-  double Sphere(double radius);
-  double Pyramid(double height, double area);
-  double Cylinder(double height, double radius);
+  double cube(const double side);
+  double sphere(const double radius);
+  double pyramid(const double height, const double area);
+  double cylinder(const double height, const double radius);
 };
 
 #endif  // CODE_SIROTIN_NIKITA_LABA2_INCLUDE_VOLUMEFIGURES_H_
