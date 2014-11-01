b7141913e839683f7eb1fa0cdbeb99f7b539167f
Nikita94 <niksir20@mail.ru>
2014-11-01 13:25:14 +0400

    Fixed test_volume_figrures.cpp file

--- code/sirotin-nikita-laba2/test/test_volume_figrures.cpp
+++ code/sirotin-nikita-laba2/test/test_volume_figrures.cpp
@@ -17,76 +17,87 @@ class VolumeTest : public ::testing::Test {
 };
 
 TEST_F(VolumeTest, Cube) {
-    EXPECT_EQ(8.0, vol.Cube(2.0));
+    EXPECT_NEAR(8.0, vol.cube(2.0), 1e-6);
 }
 
 TEST_F(VolumeTest, Do_Throw_Exception_When_Volume_Is_Larger_Than_DBL_MAX_Cube) {
-    EXPECT_THROW(vol.Cube(DBL_MAX), std::string);
+    EXPECT_THROW(vol.cube(DBL_MAX), std::string);
 }
 
 TEST_F(VolumeTest,
-Do_Throw_Exception_When_Volume_Is_Larger_Than_Minus_DBL_MAX_Cube) {
-    EXPECT_THROW(vol.Cube(-DBL_MAX), std::string);
+Do_Throw_Exception_When_Variable_Is_Less_Than_Zero_Cube) {
+    EXPECT_THROW(vol.cube(-1.), std::string);
 }
 
 TEST_F(VolumeTest,
 Do_Throw_Exception_When_Volume_Is_Zero_Cube) {
-    EXPECT_THROW(vol.Cube(0), std::string);
+    EXPECT_THROW(vol.cube(0.), std::string);
 }
 
 TEST_F(VolumeTest, Sphere) {
-    EXPECT_EQ(M_PI * 4 / 3, vol.Sphere(1.));
+    EXPECT_NEAR(M_PI * 4. / 3., vol.sphere(1.), 1e-6);
 }
 
 TEST_F(VolumeTest,
 Do_Throw_Exception_When_Volume_Is_Larger_Than_DBL_MAX_Sphere) {
-    EXPECT_THROW(vol.Sphere(DBL_MAX), std::string);
+    EXPECT_THROW(vol.sphere(DBL_MAX), std::string);
 }
 
 TEST_F(VolumeTest,
-Do_Throw_Exception_When_Volume_Is_Larger_Than_Minus_DBL_MAX_Sphere) {
-    EXPECT_THROW(vol.Sphere(-DBL_MAX), std::string);
+Do_Throw_Exception_When_Variable_Is_Less_Than_Zero_Sphere) {
+    EXPECT_THROW(vol.sphere(-1.), std::string);
 }
 
 TEST_F(VolumeTest,
 Do_Throw_Exception_When_Volume_Is_Zero_Sphere) {
-    EXPECT_THROW(vol.Sphere(0), std::string);
+    EXPECT_THROW(vol.sphere(0.), std::string);
 }
 
 TEST_F(VolumeTest, Pyramid) {
-    EXPECT_EQ(4., vol.Pyramid(3., 4.));
+    EXPECT_NEAR(4., vol.pyramid(3., 4.), 1e-6);
 }
 
 TEST_F(VolumeTest,
 Do_Throw_Exception_When_Volume_Is_Larger_Than_DBL_MAX_Pyramid) {
-    EXPECT_THROW(vol.Pyramid(DBL_MAX, DBL_MAX), std::string);
+    EXPECT_THROW(vol.pyramid(DBL_MAX, DBL_MAX), std::string);
 }
 
 TEST_F(VolumeTest,
-Do_Throw_Exception_When_Volume_Is_Larger_Than_Minus_DBL_MAX_Pyramid) {
-    EXPECT_THROW(vol.Pyramid(-DBL_MAX, -DBL_MAX), std::string);
+Do_Throw_Exception_When_Variable_Is_Less_Than_Zero_Pyramid_1) {
+    EXPECT_THROW(vol.pyramid(-1., 5.), std::string);
 }
+
+TEST_F(VolumeTest,
+Do_Throw_Exception_When_Variable_Is_Less_Than_Zero_Pyramid_2) {
+    EXPECT_THROW(vol.pyramid(1., -5.), std::string);
+}
+
 
 TEST_F(VolumeTest,
 Do_Throw_Exception_When_Volume_Is_Zero_Pyramid) {
-    EXPECT_THROW(vol.Pyramid(0, 23), std::string);
+    EXPECT_THROW(vol.pyramid(0., 23.), std::string);
 }
 
 TEST_F(VolumeTest, Cylinder) {
-    EXPECT_EQ(3. * M_PI, vol.Cylinder(3., 1.));
+    EXPECT_NEAR(3. * M_PI, vol.cylinder(3., 1.), 1e-6);
 }
 
 TEST_F(VolumeTest,
 Do_Throw_Exception_When_Volume_Is_Larger_Than_DBL_MAX_Cylinder) {
-    EXPECT_THROW(vol.Cylinder(DBL_MAX, DBL_MAX), std::string);
+    EXPECT_THROW(vol.cylinder(DBL_MAX, DBL_MAX), std::string);
 }
 
 TEST_F(VolumeTest,
-Do_Throw_Exception_When_Volume_Is_Larger_Than_Minus_DBL_MAX_Cylinder) {
-    EXPECT_THROW(vol.Cylinder(-DBL_MAX, -DBL_MAX), std::string);
+Do_Throw_Exception_When_Variable_Is_Less_Than_Zero_Cylinder_1) {
+    EXPECT_THROW(vol.cylinder(-1., 3.), std::string);
+}
+
+TEST_F(VolumeTest,
+Do_Throw_Exception_When_Variable_Is_Less_Than_Zero_Cylinder_2) {
+    EXPECT_THROW(vol.cylinder(1., -3.), std::string);
 }
 
 TEST_F(VolumeTest,
 Do_Throw_Exception_When_Volume_Is_Zero_Cylinder) {
-    EXPECT_THROW(vol.Cylinder(0, 23), std::string);
+    EXPECT_THROW(vol.cylinder(0., 23.), std::string);
 }
