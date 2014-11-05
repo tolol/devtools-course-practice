45c3d715dd7fb10922d8f9249d89c4888d7e078e
Julia Buchina <julia.buchina@mail.ru>
2014-11-05 19:40:26 +0400

    Fixed fraction.h

--- code/buchina-julia/include/fraction.h
+++ code/buchina-julia/include/fraction.h
@@ -7,17 +7,23 @@ class Fraction {
  private:
     int numerator;
     int denominator;
+ protected:
+    void setNumerator(int _numerator);
+    void setDenominator(int _denominator);
  public:
-     Fraction();
-     Fraction(int _numerator, int _denominator);
-     Fraction(const Fraction& fraction);
-     ~Fraction();
+    Fraction();
+    Fraction(int _numerator, int _denominator);
+    Fraction(const Fraction& fraction);
+    ~Fraction();
 
-     int getNumerator();
-     int getDenominator();
+    int getNumerator();
+    int getDenominator();
 
-     Fraction& operator=(const Fraction& fraction);
-     Fraction operator+(const Fraction& a, const Fraction& b);
+    const Fraction& operator=(const Fraction& fraction);
+    Fraction& operator+(const Fraction& a);
+    Fraction& operator-(const Fraction& a);
+    Fraction& operator*(const Fraction& a);
+    Fraction& operator/(const Fraction& a);
 };
 
-#endif
+#endif  // CODE_BUCHINA_JULIA_INCLUDE_FRACTION_H_
