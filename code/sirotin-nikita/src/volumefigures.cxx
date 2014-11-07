87e174e04cb30eb676f4e7c8f38cc0038647aba2
Nikita94 <niksir20@mail.ru>
2014-11-07 18:10:12 +0400

    Fixed volumefigures.cxx file

---
+++ code/sirotin-nikita/src/volumefigures.cxx
@@ -0,0 +1,63 @@
+// Copyright 2014 Sirotin Nikita
+
+#include "include/volumefigures.h"
+
+#include <stdint.h>
+#include <math.h>
+#include <float.h>
+#include <string>
+
+void CheckValue(double value);
+void CheckRange(double result);
+
+void CheckValue(double value) {
+  if (value < 0.)
+    throw std::string("The variable can not be less than zero");
+}
+
+void CheckRange(double result) {
+    if (DBL_MAX - result < 1e-10)
+        throw std::string("The result is larger than DBL_MAX");
+    if (result < 1e-10)
+      throw std::string("There are zero variables");
+}
+
+double Volume::cube(const double side) {
+  CheckValue(side);
+
+  double vol = pow(side, 3);
+  CheckRange(vol);
+
+  return vol;
+}
+
+double Volume::sphere(const double radius) {
+  CheckValue(radius);
+
+  double vol = 4. * M_PI * pow(radius, 3) / 3.;
+  CheckRange(vol);
+
+  return vol;
+}
+
+double Volume::pyramid(const double height, const double area) {
+  CheckValue(height);
+  CheckValue(area);
+
+  double vol = area * height / 3.;
+  CheckRange(vol);
+
+  return vol;
+}
+
+double Volume::cylinder(const double height, const double radius) {
+  CheckValue(height);
+  CheckValue(radius);
+
+  double area = radius * radius * M_PI;
+  CheckRange(area);
+  double vol = area * height;
+  CheckRange(vol);
+
+  return vol;
+}
